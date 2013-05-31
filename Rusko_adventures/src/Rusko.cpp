8775834ff955868eef4f8dd9398190b96f0dfbe4
Pam <ipam73@gmail.com>
2013-05-30 19:58:51 -0700

    Deleted annoying prints

--- Rusko_adventures/src/Rusko.cpp
+++ Rusko_adventures/src/Rusko.cpp
@@ -2,8 +2,6 @@
 #define _USE_MATH_DEFINES
 
 #include "Rusko.h"
-#include <stdio.h>
-#include <iostream.h>
 
 using namespace std;
 
@@ -11,14 +9,10 @@ Rusko::Rusko() {
     model = new Model("models/rusko/Rusko_full_03.obj");
     //to load animation
     for (int i = 2; i <= lastFrame_run; i++){
-
         string mod_name = "models/rusko_run/Rusko_ani_03_0000";
         mod_name += (i/10 + '0');
         mod_name += ((i % 10) +'0');
         mod_name += ".obj";
-        
-        cout << mod_name << endl;
-        
         Model* mod = new Model(mod_name);
         renderList.push_back(mod);
     }
@@ -30,6 +24,5 @@ Rusko::~Rusko() {
 
 void Rusko::render(int frame) {
     frame = frame % renderList.size();
-    printf("frame is %i \n", frame);
     renderList.at(frame)->render();
 }
