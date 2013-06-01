0604ee3d6a1ec2dd82724ad21f3b5b078988dd91
Aaron Damashek <aarond@stanford.edu>
2013-05-31 17:54:05 -0700

    Got recorded fire working

--- Rusko_adventures/include/Emitter.h
+++ Rusko_adventures/include/Emitter.h
@@ -21,17 +21,28 @@
 #endif
 
 #include "ParticleStructs.h"
+#include <vector.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
 
 class Emitter
 {
 public:
 	Emitter(particle **pool, int emitter_id, vector3 pos, vector3 dir, vector3 dirVar, float speed, float speedVar, int totalParticles, int emitsPerFrame, int emitVar, int life, int lifeVar, vector3 force);
+    Emitter(particle **pool, int emitter_id, string filepath);
     ~Emitter();
     void update();
     virtual void display();
     void resetPos(vector3 newPos);
     void setEmitting(bool emit);
     void setDisplaying(bool display);
+    
+    virtual void recordEmission(string outputFile);
+    virtual void loadEmission(string filepath);
+    void updateFromFile();
+    
 protected:
     particle **managerParticleList;
     emitter *e;
@@ -41,6 +52,15 @@ protected:
     inline float randDist();
     bool emitting;
     bool displaying;
+    ofstream *emitterInfo;
+    bool recording;
+    bool playFromFile;
+    vector< vector <vector3> > storedLocations; // Particles / Frames for particle / vector for frame
+    string intToString(int num);
+    string floatToString(float num);
+    float readFloat(ifstream *infile);
+    int readInt(ifstream *infile);
+    
 };
 
 
