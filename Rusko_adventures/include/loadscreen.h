3be26185cbd68efad96c14e12aec9569ba9d8250
Pam <ipam73@gmail.com>
2013-06-05 22:23:04 -0700

    Better loading screen

--- Rusko_adventures/include/loadscreen.h
+++ Rusko_adventures/include/loadscreen.h
@@ -14,15 +14,23 @@
 #include "Framework.h"
 #include "Renderable.h"
 
+using namespace std;
+
 class Loadscreen {
 public:
 	Loadscreen();
 	~Loadscreen();
-    void render(int level);
+    void render(int level, int windowWidth, int windowHeight);
     
 private:
     void renderLevel(int level);
 	void initScreen();
+    void drawIntroScreen();
+    void drawMessageAt(string message, float xpos, float ypos, STColor4f color);
+    STFont* font;
+    
+    int windowWidth;
+    int windowHeight;
 };
 
 #endif /* defined(__Rusko_adventures__loadscreen__) */
--- Rusko_adventures/src/loadscreen.cpp
+++ Rusko_adventures/src/loadscreen.cpp
@@ -16,46 +16,76 @@ Loadscreen::~Loadscreen(){
     
 }
 
-void Loadscreen::render(int level){
-    renderLevel(level);
-}
-
-
-void Loadscreen::renderLevel(int level){
-    
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
+void Loadscreen::render(int level, int windowWidth, int windowHeight){
+    this->windowWidth = windowWidth;
+    this->windowHeight = windowHeight;
     
     // Clear the window.
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     
-    // Draw background
-//    glEnable(GL_TEXTURE_2D);
-//
-//    glDisable(GL_DEPTH_TEST);
-//    glDepthMask(GL_FALSE);
-//    
-//   
-//    static STFont* gFont = new STFont("resources/arial.ttf", 24);
-//    gFont->DrawString("hello", STColor4f(0, 0, 0, 1));  //Draws the string
-
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
     
-    //printf("level %i", level);
-    glBegin(GL_QUADS);
-    glColor3f(1.0, 0, 0);
-    glVertex2f(0, 0);
-    glVertex2f(0, .5);
-    glVertex2f(.5, .5);
-    glVertex2f(.5, 0);
-    glEnd();
+    if (level == 1) drawIntroScreen();
+    else renderLevel(level);
     
     glPopMatrix();
     glDisable(GL_TEXTURE_2D);
-    
 }
 
+
+void Loadscreen::renderLevel(int level){
+    
+    string message = "New Level";
+    drawMessageAt(message, 0, 80, STColor4f(1, 1, 1, 1));
+
+}
+
+
+void Loadscreen::drawIntroScreen(){
+
+    string welcome = "Welcome to Rusko's Aventures";
+    string instructions = "Press enter to start";
+    drawMessageAt(welcome, 0, 80, STColor4f(1, 1, 1, 1));
+    drawMessageAt(instructions, 0, 30, STColor4f(1, 1, 1, 1));
+    
+    glBegin(GL_TRIANGLES);
+    glColor3f(1, 0, 1);
+    glVertex2d(0, 0);
+    glColor3f(1, 0, 1);
+    glVertex2d(0, 1);
+    glColor3f(1, 0, 1);
+    glVertex2d(1, 1);
+    glEnd();
+}
 
 void Loadscreen::initScreen(){
-    
+    font = new STFont("resources/arial.ttf", 32);
 }
+
+
+
+void Loadscreen::drawMessageAt(string message, float xpos, float ypos, STColor4f color){
+    
+    glViewport(0, 0, windowWidth, windowHeight);
+    glMatrixMode( GL_PROJECTION );
+    glLoadIdentity();
+    glOrtho(-windowWidth/2, windowWidth/2, -windowHeight/2, windowHeight/2, -1., 1.);
+
+    
+    float pixelsPerUnit = GLUT_WINDOW_WIDTH/(float)windowWidth;
+    
+    
+    float temp = font->ComputeWidth(message);
+    
+    xpos = xpos - (float)temp/2;
+    //ypos = ypos - (float)font->GetAscender()/2;
+    glTranslatef(xpos, ypos, 0);
+    font->DrawString(message, color);  //Draws the string
+
+    
+    
+    glPopMatrix ();
+}
+
